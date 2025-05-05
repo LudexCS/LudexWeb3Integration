8a6d39afe1e2f6b371787a09b8fc44ae3429f906
ShyBoy <tjghdudrnt@naver.com>
2025-05-06 01:06:10 +0900

    fix: Fix typo (store.ts)

--- src/ts/adapters/store.ts
+++ src/ts/adapters/store.ts
@@ -49,7 +49,7 @@ export class MetaTXAdapterStore
             {name: "spender", type: "address"},
             {name: "value", type: "uint256"},
             {name: "nonce", type: "uint256"},
-            {name: "deadline", type: "deadline"}
+            {name: "deadline", type: "uint256"}
          ]
       };
 
